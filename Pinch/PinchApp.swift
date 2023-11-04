6ca355463400c7522870488972b2ae596db3a53e
Antoni Gebauer <antoni.gebauer@comarch.com>
2023-11-04 19:39:10 +0100

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Antoni Gebauer on 04/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
